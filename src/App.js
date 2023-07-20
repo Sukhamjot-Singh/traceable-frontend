05360646fd3548e748290d7f0a52b4da5d869c0b
sukhamjot-singh <sukhamjot2001@gmail.com>
2023-07-20 11:59:49 +0530

    uploading

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+
+import { useState, useEffect } from "react";
 import './App.css';
 
 function App() {
+  useEffect(() => { 
+      // fetch("http://localhost:5000/getContainers")
+      //   .then(response => {
+      //     return response.json();
+      //   })
+      //   .then(data => {
+      //     console.log(data);
+      //   })
+  }, []);
+  const [message, setMessage] = useState('');
+  const [save, setSave] = useState(false);
+
+  const handleChange = event => {
+    setMessage(event.target.value);
+    if(event.target.value != '')
+     {setSave(true);}
+    console.log('value is:', event.target.value);
+  };
+
+  const handleSubmit = event => {
+
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        {save?(<button className="bg-primary text-white" onClick={handleSubmit}>Save</button>) : <></>}
+      <div className="row">
+        <div className="col-md-6 text-center">
+          <input className="m-3 form-control" placeholder="Type input"
+            onChange={handleChange}
+            value={message}></input>
+            <h3>{message}</h3>
+        </div>
+        <div className="col-md-6 text-center">
+        <input className="m-3" type="checkbox"></input>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-6 text-center">
+          <input className="m-3" type="textarea"></input>
+        </div>
+        <div className="col-md-6 text-center">
+          <input className="m-3" type="checkbox"></input>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-6 text-center">
+          Yo
+        </div>
+        <div className="col-md-6 text-center">
+          Yo
+        </div>
+      </div>
     </div>
   );
 }
